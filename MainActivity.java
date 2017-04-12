29b402e10868415e709469f53ad0958a175177a0
nive18 <niveditha.itengineer@gmail.com>
2017-04-12 17:41:35 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,21 @@
+package tweety.app.com.favouritetoys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mToysListTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mToysListTextView = (TextView)findViewById(R.id.tv_toy_names);
+
+    String toyNames [] = ToyBox.getToyNames();
+    for(String toyName:toyNames)
+    {
+       mToysListTextView.append(toyName+"\n\n\n");
+    }
+}
+}
